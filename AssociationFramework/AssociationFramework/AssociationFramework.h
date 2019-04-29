5e7ed0725b123549e78815ca4a84961791ee2ddc
Justin Kaufman <jmkauf@gmail.com>
2019-04-28 18:12:20 -0700

    Xcode build AssociationShared via submodule

---
+++ AssociationFramework/AssociationFramework/AssociationFramework.h
@@ -0,0 +1,19 @@
+//
+//  AssociationFramework.h
+//  AssociationFramework
+//
+//  Created by Justin Kaufman on 4/28/19.
+//  Copyright © 2019 MakeGoodStuff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AssociationFramework.
+FOUNDATION_EXPORT double AssociationFrameworkVersionNumber;
+
+//! Project version string for AssociationFramework.
+FOUNDATION_EXPORT const unsigned char AssociationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssociationFramework/PublicHeader.h>
+
+
